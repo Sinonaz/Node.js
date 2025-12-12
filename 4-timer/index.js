ddef7b1a59c4a6d256935e85bcb20e6415c3644b
Isaac <aziksimpson@gmail.com>
2025-12-12 17:39:56 +0300

    fix timer

--- 4-timer/index.js
+++ 4-timer/index.js
@@ -1,8 +1,28 @@
-const hours = Number((process.argv[2] ?? 0) * 3600000)
-const minutes = Number((process.argv[3] ?? 0) * 60000)
-const seconds = Number((process.argv[4] ?? 0) * 1000)
+const string = process.argv[2]
 
-const timeout = hours + minutes + seconds
+if (!string) {
+  console.log('Строка не передана')
+}
+
+const stringSplit = string.split(' ')
+
+const timeout = stringSplit.reduce((acc, item) => {
+  let ml
+
+  switch (true) {
+    case item.includes('h'):
+      ml = Number(item.replaceAll('h', '')) * 3600000
+      break
+    case item.includes('m'):
+      ml = Number(item.replaceAll('m', '')) * 60000
+      break
+    case item.includes('s'):
+      ml = Number(item.replaceAll('s', '')) * 1000
+      break
+  }
+
+  return acc + ml
+}, 0)
 
 setTimeout(() => {
   console.log(`Прошло ${timeout} милисекунд`)
